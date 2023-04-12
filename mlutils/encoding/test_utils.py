c73879743e9ecfff503037307011ecd7f434fa04
Bogdan Gulowaty <b.gulowaty@gmail.com>
2023-04-12 23:06:06 +0200

    Add train-test encoding util

--- mlutils/encoding/test_utils.py
+++ mlutils/encoding/test_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from mlutils.encoding.utils import encode_columns_to_labels
+from mlutils.encoding.utils import encode_columns_to_labels, encode_train_test_to_labels
 
 
 def test_encodes_correctly():
@@ -18,3 +18,25 @@ def test_encodes_correctly():
         [0, 0],
         [1, 0]
     ]]).all()
+
+
+def test_train_test_encodes_correctly():
+    # given
+    train = np.array([
+        ['a', 'b'],
+        ['c', 'a'],
+    ])
+    test = np.array([
+        ['c', 'b'],
+        ['a', 'e'],
+    ])
+
+    # when
+    train_encoded, test_encoded = encode_train_test_to_labels(train, test)
+
+    # then
+    assert train_encoded[0, 0] == test_encoded[1, 0]
+    assert train_encoded[1, 0] == test_encoded[0, 0]
+    assert train_encoded[0, 1] == test_encoded[0, 1]
+    assert train_encoded[0, 1] != test_encoded[1, 1]
+    assert train_encoded[1, 1] != test_encoded[1, 1]
--- mlutils/encoding/utils.py
+++ mlutils/encoding/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
+from mlutils.encoding.refittable_label_encoder import RefitableLabelEncoder
+
 
 def encode_columns_to_labels(numpy_array):
     if len(numpy_array.shape) != 2:
@@ -9,3 +11,18 @@ def encode_columns_to_labels(numpy_array):
     le = LabelEncoder()
 
     return np.apply_along_axis(lambda col: le.fit_transform(col), 0, numpy_array)
+
+def encode_train_test_to_labels(train_dataset, test_dataset):
+    if len(train_dataset.shape) != 2 or len(test_dataset.shape) != 2:
+        raise Exception("Only 2d arrays are supported")
+
+    encoders = [RefitableLabelEncoder() for _ in range(train_dataset.shape[1])]
+
+    encoders_iterator = iter(encoders)
+    train_encoded = np.apply_along_axis(lambda col: next(encoders_iterator).fit_transform(col), 0, train_dataset)
+
+    encoders_iterator = iter(encoders)
+    test_encoded = np.apply_along_axis(lambda col: next(encoders_iterator).fit_transform(col), 0, test_dataset)
+
+    return train_encoded, test_encoded
+
